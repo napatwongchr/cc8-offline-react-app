a011fc277c1a05182e6bbc9741a867d22797f245
Napat <napat.wongchr@gmail.com>
2021-03-30 23:29:58 +0700

    refactor: button to sizes and variants

--- my-app/src/components/DesignSystem/Buttons.js
+++ my-app/src/components/DesignSystem/Buttons.js
@@ -8,42 +8,29 @@ function Buttons() {
       </Heading>
 
       <Box mb="5">
-        <Heading as="h2" size="md" mb="5">
-          sm size
+        <Heading as="h2" size="lg" mb="5">
+          Sizes
         </Heading>
-        <Flex justify="space-between" width="40%">
-          <Button size="sm">Start Learning Now</Button>
-          <Button size="sm" variant="secondary">
-            Sign up
-          </Button>
-          <Button size="sm" variant="outline">
-            Login
-          </Button>
+        <Flex justify="space-between" alignItems="center" width="40%">
+          <Button size="sm">Size sm</Button>
+          <Button size="md">Size md</Button>
+          <Button size="lg">Size lg</Button>
         </Flex>
       </Box>
 
       <Box mb="5">
-        <Heading as="h2" size="md" mb="5">
-          md size
+        <Heading as="h2" size="lg" mb="5">
+          Variants
         </Heading>
         <Flex justify="space-between" width="40%">
-          <Button>Start Learning Now</Button>
-          <Button variant="secondary">Sign up</Button>
-          <Button variant="outline">Login</Button>
-        </Flex>
-      </Box>
-
-      <Box mb="5">
-        <Heading as="h2" size="md" mb="5">
-          lg size
-        </Heading>
-        <Flex justify="space-between" width="40%">
-          <Button size="lg">Start Learning Now</Button>
-          <Button size="lg" variant="secondary">
-            Sign up
+          <Button size="md" variant="primary">
+            Primary
           </Button>
-          <Button size="lg" variant="outline">
-            Login
+          <Button size="md" variant="secondary">
+            Secondary
+          </Button>
+          <Button size="md" variant="outline">
+            Outline
           </Button>
         </Flex>
       </Box>
